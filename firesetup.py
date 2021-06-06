f9de189d20683a2a977876bf6ba04a9d101c1ffe
Manas Hejmadi | Developer <67999871+synapsecode@users.noreply.github.com>
2021-06-07 02:03:45 +0530

    FBSetupMode+PreventRerun

--- firesetup.py
+++ firesetup.py
@@ -2,23 +2,43 @@ import os
 import sys
 import argparse
 
+#Constants
+VERSION_NUMBER = "0.4.0"
+SCC = 75 #SeperatorCharacterCount
+
+
+#Checks if the Current Directory is a Flutter Project
+def is_flutter_project(directory):
+	directory = directory.replace('/', '\\').replace('\\', '//')
+	#Check for pubspec.yaml file and the android folder
+	pubspec_check = os.path.exists(os.path.join(directory, 'pubspec.yaml'))
+	android_check = os.path.exists(os.path.join(directory, 'android'))
+	# print(f"Directory: {directory} | Android: {android_check} | PubSpec: {pubspec_check} -> {(pubspec_check and android_check)}")
+	return (pubspec_check and android_check)
+
 def firesetup(directory, sourceDirectory, gclientid, project_name, enable_fireauth):
-	print(75*'=')
-	print("Flutter Firebase Setup Utility v0.1.0!")
-	print(f"Executing FireSetup on Flutter Project: {project_name}")
-	print("using googleClientID:", gclientid)
+	print('\n' + SCC*'-')
+	print(f"FireSetup(v{VERSION_NUMBER}) running in Firebase Setup Mode")
+	print(f"Executing FireSetup on Flutter Project: '{project_name}'")
+	print("Using GoogleSignInClientID:", gclientid)
 
 	# Check if FireSetup is in a Flutter Project
-	if(not os.path.exists(os.path.join(directory, 'pubspec.yaml'))):
-		print(75*'-')
+	if(not is_flutter_project(directory)):
+		print(SCC*'-')
 		print("Fatal Error: FireSetup is not Inside a Flutter Project")
 		print("Please run the FireSetup Command in a Flutter Project directory")
-		print(75*'-')
+		print(SCC*'-')
 		return
 	else:
-		print(75*'-')
+		print(SCC*'-')
 
-
+	#Check if FireSetup has already been used, preventing double usage
+	with open(os.path.join(directory, 'android', 'app', 'build.gradle')) as x:
+		src = x.read()
+		if("implementation platform('com.google.firebase:firebase-bom:27.1.0')" in src):
+			print("FireSetup has already been used on this project!")
+			print(SCC*'-')
+			return
 	#--------------------------------------------------------------------------------------------------
 	with open(os.path.join(directory, 'web', 'index.html'), 'w+') as f:
 		src = f.read()
@@ -100,16 +120,105 @@ def firesetup(directory, sourceDirectory, gclientid, project_name, enable_fireau
 			with open(os.path.join(directory, 'lib/main.dart'), 'w') as f:
 				f.write(x.read())
 		print("Replaced main.dart with FireAuth Example")
-	#--------------------------------------------------------------------------------------------------	
-	print(75*'-')
+
+	print(SCC*'-')
 	print("Firebase Setup Done!")
-	print(75*'=')
-	print("Final Steps")
-	print("Replace the placeholder firebaseConfig object in web/index.html")
-	print(" with the one you copied from Add Firebase Web App Operation")
-	print("Please Enable the Required AuthenticationMethods in your Firebase Auth Console")
-	print("run flutter pub get, write some code and then run the application")
-	print(75*'=')
+	print(SCC*'-')
+	print("Final Steps\n")
+	print("1) Replace the placeholder firebaseConfig object in web/index.html")
+	print("   with the one you copied from the 'Add Firebase to Web App' Operation")
+	print("2) Enable the Required AuthProviders in your Firebase Auth Console")
+	print("3) For Social Auth Providers and Phone Auth visit (https://bit.ly/3pvWNJR)")
+	print("   and follow the instructions for whichever AuthProvider you want!")
+	print("4) run the 'flutter pub get' command")
+	print(SCC*'-')
+
+
+def facebook_setup(directory, sourceDirectory, fbid, project_name, fbname):
+	print('\n' + SCC*'-')
+	print(f"FireSetup(v{VERSION_NUMBER}) running in FacebookAuth Setup Mode")
+	print(f"Executing FacebookAuthSetup on Flutter Project: '{project_name}'")
+	print("using FacebookAppID:", fbid)
+
+	# Check if FireSetup is in a Flutter Project
+	if(not is_flutter_project(directory)):
+		print(SCC*'-')
+		print("Fatal Error: FacebookAuthSetup is not Inside a Flutter Project")
+		print("Please run the FireSetup Command in a Flutter Project directory")
+		print(SCC*'-')
+		return
+	else:
+		print(SCC*'-')
+
+	#--------------------------------------------------------------------------------------------------
+
+	content = f'''<?xml version="1.0" encoding="utf-8"?>
+<resources>
+	<string name="app_name">{fbname}</string>
+	<string name="facebook_app_id">{fbid}</string>
+	<string name="fb_login_protocol_scheme">fb{fbid}</string>
+</resources>'''
+
+	with open(os.path.join(directory, 'android', 'app', 'src', 'main', 'res', 'values', 'strings.xml'), 'w') as f:
+		f.write(content)
+
+	print("Created a strings.xml Resource File")
+	#--------------------------------------------------------------------------------------------------
+	manifest_path = os.path.join(directory, 'android', 'app', 'src', 'main', 'AndroidManifest.xml')
+	with open(manifest_path) as f:
+		src = f.read()
+		newsrc = src
+		#If Doesn't already contain Internet Permission
+		if('<uses-permission android:name="android.permission.INTERNET"/>' not in newsrc):
+			newsrc = src.replace(
+				#Add Internet Permissions
+				'   <application',
+				'   <uses-permission android:name="android.permission.INTERNET"/>\n   <application'
+			)
+
+
+		fbsdk_content = '''\n        <!-- ====================FACEBOOK LOGIN SETUP====================== -->
+        <meta-data 
+          android:name="com.facebook.sdk.ApplicationId" 
+          android:value="@string/facebook_app_id"
+        />
+        <activity 
+          android:name="com.facebook.FacebookActivity"
+          android:configChanges= "keyboard|keyboardHidden|screenLayout|screenSize|orientation"
+          android:label="@string/app_name"
+        />
+        <activity
+          android:name="com.facebook.CustomTabActivity"
+          android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.VIEW" />
+                <category android:name="android.intent.category.DEFAULT" />
+                <category android:name="android.intent.category.BROWSABLE" />
+                <data android:scheme="@string/fb_login_protocol_scheme" />
+            </intent-filter>
+        </activity>
+        <!-- ====================FACEBOOK LOGIN SETUP====================== -->\n'''
+
+		if('@string/fb_login_protocol_scheme' not in newsrc):
+			newsrc = newsrc.replace(
+				#Add Facebook Integration
+				'    </application>',
+				fbsdk_content+'    </application>'
+			)
+		else:
+			print("Facebook Integration has already been performed!")
+			print(SCC*'-')
+			return
+
+		#Saving Changes
+		with open(manifest_path, 'w') as x:
+				x.write(newsrc)
+	print("Completed Facebook Integration with AndroidManifest.xml")
+	#--------------------------------------------------------------------------------------------------
+
+	print(SCC*'-')
+	print("Facebook Setup Done!")
+	print(SCC*'-')
 
 
 if(__name__ == '__main__'):
@@ -118,6 +227,11 @@ if(__name__ == '__main__'):
 	parser.add_argument('directory', help="Target Directory", type= str)
 	parser.add_argument('--gclientid', '-gcid', help="Google Client ID", type= str)
 	parser.add_argument('--enable_fireauth', '-efa', help="Add FireAuth to Project along With a Template", type= str, default="False")
+	
+	#Facebook Setup
+	parser.add_argument('--facebook_app_id', '-fbid', help="Facebook App ID", type=str, default="")
+	parser.add_argument('--facebook_app_name', '-fbname', help="Facebook App Name", type=str, default="FacebookAuthApp")
+
 	args = parser.parse_args()
 
 	cloc = args.cloc
@@ -125,7 +239,16 @@ if(__name__ == '__main__'):
 	gclientid = args.gclientid
 	enable_fireauth = True if args.enable_fireauth == "True" else False
 
-	project_name = directory.split('\\')[-1]
+	fbid=args.facebook_app_id
+	fbname = args.facebook_app_name
 
+
+	project_name = directory.split('\\')[-1]
 	sourceDirectory = os.path.join(cloc, 'src')
-	firesetup(directory, sourceDirectory, gclientid, project_name, enable_fireauth)
+
+	if(fbid == ""):
+		#Regular FireSetup
+		firesetup(directory, sourceDirectory, gclientid, project_name, enable_fireauth)
+	else:
+		#Facebook FireSetup
+		facebook_setup(directory, sourceDirectory, fbid, project_name, fbname)
